b482e34ba7be67503c85a49505114e3da9f24b12
Richard Kim <richardjkim7@gmail.com>
2022-11-21 14:33:04 -0600

    repo and JPA

---
+++ src/main/java/com/codeup/HelloController.java
@@ -0,0 +1,62 @@
+package com.codeup;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Controller
+//    class HelloController {
+//        @GetMapping("/hello")
+//        @ResponseBody
+//        public String hello() {
+//            return "Hello from Spring!";
+//        }
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name) {
+//        return "Hello " + name + "!";
+//    }
+//    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String addOne(@PathVariable int number) {
+//        return number + " plus one is " + (number + 1) + "!";
+//    }
+////    @GetMapping("/hello/{name}")
+////    public String sayHello(@PathVariable String name, Model model) {
+////        model.addAttribute("name", name);
+////        return "hello";
+////    }
+//    }
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+}
+
