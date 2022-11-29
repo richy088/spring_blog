7c9f387542ccac484ec11b9624cc8a369f26a705
Richard Kim <richardjkim7@gmail.com>
2022-11-29 09:05:20 -0600

    security spring exercises

---
+++ src/main/java/com/codeup/AuthenticationController.java
@@ -0,0 +1,25 @@
+package com.codeup;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthenticationController {
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam(name = "userName") String userName, @RequestParam(name = "password") String password, Model model) {
+        return "redirect:/posts";
+    }
+
+    @PostMapping("/logout")
+    public String logout() {
+        return "users/login";
+    }
+}
--- src/main/java/com/codeup/User.java
+++ src/main/java/com/codeup/User.java
@@ -10,15 +10,24 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 20, unique = true)
     private String userName;
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 20, unique = true)
     private String email;
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false)
     private String password;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Post> posts;
+
+    public User() {}
+
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        userName = copy.userName;
+        password = copy.password;
+    }
 
     public List<Post> getPosts() {
         return posts;
